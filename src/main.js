49f5d48eb7c0634fed72700a0a74519ab2a94522
marcelohmdias <marcelohmdias@gmail.com>
2018-05-28 12:01:10 -0300

    Alterando organização dos arquivos.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import Vue from 'vue'
-import App from './App.vue'
+import { sync } from 'vuex-router-sync'
+
+// Modules
 import router from './router'
 import store from './store'
+
+// Application
+import App from './App'
 import './registerServiceWorker'
 
+sync(store, router)
+
+const isDev = process.env.NODE_ENV !== 'production'
+
+const render = h => h(App)
+
+const app = new Vue({ router, store, render })
+
+Vue.config.devtools = isDev
+Vue.config.performance = isDev
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+app.$mount('[data-app]')
---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+
+Vue.use(VueRouter)
+
+const mode = 'history'
+
+export default new VueRouter({ mode, routes })
